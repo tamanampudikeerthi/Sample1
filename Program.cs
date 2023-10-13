c128f1f8455a5a8087985641532198c3bd1b8ee2
USER_NAME <USER_EMAIL>
2023-10-13 10:54:41 +0530

    added a new program file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+public delegate T delOne<T>(T x,T y);
+public class delDemo
+{
+    public static void display(int x,int y)
+    {
+        Console.WriteLine(x+y);
+    }
+    public static void show(string x,string y)
+    {
+        Console.WriteLine(x+y);
+    }
+}
+public class Program
+{
+    static void Main(String[] args)
+    {
+        delOne<int> =delDemo.display;
+        delOne<string>=delDemo.show;
+        d1(20,10);
+    }
+}
+
+
